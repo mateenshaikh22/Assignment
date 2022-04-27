0f905c27dc83336a3138b54ad46c90a044480628
mateenshaikh22 <mateenshaikh640640@gmail.com>
2022-04-27 11:37:30 +0530

    new change or some error fix

--- BackendDemoProject/index.js
+++ BackendDemoProject/index.js
@@ -2,7 +2,7 @@
  * Created by web on 11/2/17.
  */
 
-var nodeServer = require('./node_server');
+var nodeServer = require('./server');
 var mongo = require('./db_connections/mongo_connection');
 nodeServer.server;
 mongo.mongoConnection();
